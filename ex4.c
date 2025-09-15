60c64f21871a2127b9cfd749af6454d276d939bb
Gui <guilherme.s.dapi@gmail.com>
2025-09-15 16:25:04 -0300

    ex4 update podi

--- ex4.c
+++ ex4.c
@@ -1,7 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int*** aloca_matriz (int m, int n, int z) {
+    int ***matriz = (int***) malloc(m*sizeof(int**));
+    if (matriz == NULL) {
+        return NULL;
+    }
 
+    for (int i = 0; i < m; i++) {
+        matriz[i] = (int**) malloc(n*sizeof(int*));
+        for (int j = 0; j < n; j++) {
+            matriz[i][j] = (int*) malloc(z*sizeof(int));
+        }
+    }
+
+    return matriz;
+}
+
+void preenche_matriz (int m, int n, int z, int ***mat) {
+    for (int i = 0; i < z; i++) {
+        printf("Camada/Profundidade %d:\n", i);
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
+                printf("Posicao [%d][%d]: ", j, k);
+                scanf("%d", &mat[j][k][z]);
+            }
+        }
+    }
+}
+
+void imprime_matriz (int m, int n, int z, int ***mat) {
+    for (int i = 0; i < z; i++) {
+        printf("Camada/Profundidade %d:\n", i);
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < n; k++) {
+                printf("Posicao [%d][%d]:%d  ", j, k, mat[j][k][z]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
+
+int main() {
+    int m = 2, n = 2, z = 2;
+    int ***mat = aloca_matriz(m, n, z);
+    preenche_matriz(m, n, z, mat);
+    imprime_matriz(m, n, z, mat);
     return 0;
 }
